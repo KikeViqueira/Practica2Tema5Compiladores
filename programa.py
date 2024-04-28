2b1d099e73bbce193139dbad3952c39154b459a1
KikeViqueira <kiketacon@gmail.com>
2024-04-28 18:43:08 +0200

    Commit desde el pc de mesa

--- programa.py
+++ programa.py
@@ -9,7 +9,7 @@ archivo = 'medidas.xlsx'
 #Función para ejecutar el script ./nopt y capturar sus resultadps
 def ejecutar_script(i):
   resultados = []
-  for N in range( 100, 2001, 100):
+  for N in range( 100, 5001, 100):
     #Ejecutamos el script ./nopt con el valor de N actual y capturamos su salida
     salida = subprocess.run(['./nopt', str(N)] ,capture_output=True, text=True)
     tiempo= salida.stdout.strip() #extraemos el tiempo de ejecucuión limpiando los espacios en blanco
@@ -22,10 +22,10 @@ def main():
         wb = Workbook() #Creamos un nuevo libro de trabajo
         ws = wb.active #Obtenemos la hoja activa
         #Escribimos los encabezados
-        ws.append(['i'] + [str(N) for N in range(100,2001,100)])
-        #for i in range(1, 11):
-        resultados = ejecutar_script(1) #Ejecutamos el script ./nopt y capturamos sus resultados
-        ws.append([1]+resultados) #Escribimos los resultados en el archivo excel
+        ws.append(['i'] + [str(N) for N in range(100,5001,100)])
+        for i in range(1, 11):
+          resultados = ejecutar_script(1) #Ejecutamos el script ./nopt y capturamos sus resultados
+          ws.append([1]+resultados) #Escribimos los resultados en el archivo excel
         wb.save(archivo) #Guardamos el archivo excel
         print(f'Archivo {archivo} se ha actualizado con éxito')
 
